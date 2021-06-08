fa0c78dd23a8cb5e229280d850ecfab82d2a92b3
Peter Taankvist <ptaankvist@gmail.com>
2021-06-08 09:05:03 +0200

    Push cactus plot script

--- cactus_plot.py
+++ cactus_plot.py
@@ -10,7 +10,7 @@ def path_leaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
 
-def make_plot(df, num_cases, best_cases, labels, ylabel, outfile, showGraph):
+def make_plot(df, num_cases, best_cases, labels, ylabel, outfile, showGraph, linecolors, linestyles, ymax=0):
     #Remove unnamed
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
     #df = df.fillna(1000)
@@ -28,8 +28,8 @@ def make_plot(df, num_cases, best_cases, labels, ylabel, outfile, showGraph):
     cut_df.index = cut_df.index + x
     i = 0
     #labels = ['Method A (unfolded 198 nets)', 'Method B (unfolded 203 nets)', 'Method A+B (unfolded 208 nets)', 'MCC']
-    linestyles = ['-', ':', '--', '-.', '-', ':', '--', '-.']
-    linecolors = ['c', 'r', 'b', 'k', 'g','m', 'lime']
+    #linestyles = ['-', '--', ':', '-.', '-', ':', '--', '-.']
+    #linecolors = ['c', 'b', 'r', 'k', 'g','m', 'lime']
     for col in cut_df:       
         #plt.plot(cut_df[col][np.isfinite(cut_df[col])], linestyle=linestyles[i], color=linecolors[i])
         plt.plot(cut_df[col], linestyle=linestyles[i], color=linecolors[i])
@@ -45,13 +45,15 @@ def make_plot(df, num_cases, best_cases, labels, ylabel, outfile, showGraph):
     new_x = x
     while new_x < x + len(cut_df.index):
         x_values.append(new_x)
-        new_x += 10
+        new_x += 20
     x_values.append(x+ len(cut_df.index)-1)
-
+    if ymax == 0:
+        ymax = cut_df.max().max()
     plt.xticks(x_values, visible=True)
     x1,x2,y1,y2 = plt.axis()  
-    plt.axis((x,x + len(cut_df.index)-1,y1,cut_df.max().max()))
-    
+    plt.axis((x,x + len(cut_df.index)-1,y1,ymax))
+    #plt.axis((x,x + len(cut_df.index)-1,y1,10))
+    #plt.axhline(y=1, color='k', linestyle='-')
     if outfile:
         plt.savefig(outfile,bbox_inches='tight')
     if showGraph:
@@ -66,7 +68,7 @@ def get_options():
     optParser.add_option('-n', type="int", help='Number of cases to display, default=81', dest = 'numCases', default = 81)
     optParser.add_option('-o','--outSizePNG', type="string", help='Where to put the PNG for size, default=\'\'', dest = 'sizeOut', default = "")
     optParser.add_option('-p','--outTimePNG', type="string", help='Where to put the PNG for time, default=\'\'', dest = 'timeOut', default = "")
-    optParser.add_option('-d','--noshow', action="store_false", dest="show", help="display chosen graph", default=False)
+    optParser.add_option('-d','--noshow', action="store_false", dest="show", help="display chosen graph", default=True)
 
 
 
@@ -103,7 +105,7 @@ if __name__ == "__main__":
         for df in time_df_list[1:]:
             time_frame = time_frame.join(df, how='outer')
         time_frame.fillna(1000, inplace=True)
-        make_plot(time_frame, options.numCases, options.worstCases, labels, 'Time(s)', options.timeOut, options.show)
+        make_plot(time_frame, options.numCases, options.worstCases, labels, 'Time(s)', options.timeOut, options.show, ['c', 'b', 'r', 'k', 'g','m', 'lime'],['-', '--', ':', '-.', '-', ':', '--', '-.'])
     
 
     if options.doSize:
@@ -111,19 +113,32 @@ if __name__ == "__main__":
         for df in size_df_list[1:]:
             size_frame = size_frame.join(df, how='outer')
 
+        size_frame.to_csv('test.csv')
+        #size_frame[size_frame['itssize'] < size_frame['partitioningsize']].to_csv("itssizebetter.csv")
         size_df = pd.DataFrame()
         #Rules: Number/NaN = inf, NaN/Number = 0, NaN/NaN = NaN
-        size_df['A+B/mcc'] = size_frame['partitioningsize'].divide(size_frame['mccsize'], fill_value=0)
-        size_df['A+B/its'] = size_frame['partitioningsize'].divide(size_frame['itssize'], fill_value=0)
-        size_df['A+B/spike'] = size_frame['partitioningsize'].divide(size_frame['spikesize'], fill_value=0)
-        size_df['its/mcc'] = size_frame['itssize'].divide(size_frame['mccsize'], fill_value=0)
-        
+        #size_df['ITS/MCC'] = size_frame['ITSsize'].divide(size_frame['MCCsize'], fill_value=0)
+        size_df['A+B/ITS'] = size_frame['A+Bsize'].divide(size_frame['ITSsize'], fill_value=0)
+        size_df['A+B/MCC'] = size_frame['A+Bsize'].divide(size_frame['MCCsize'], fill_value=0)
+        size_df['A+B/Spike'] = size_frame['A+Bsize'].divide(size_frame['Spikesize'], fill_value=0)
+
+        #size_df = size_df[((size_df.T < 0.9) | (size_df.T > 1.1)).any()]
+        """
+        size_df['MCC/A+B'] = size_frame['MCCsize'].divide(size_frame['A+Bsize'], fill_value=0)
+        size_df['ITS/A+B'] = size_frame['ITSsize'].divide(size_frame['A+Bsize'], fill_value=0)
+        size_df['Spike/A+B'] = size_frame['Spikesize'].divide(size_frame['A+Bsize'], fill_value=0)
+        size_df['MCC/ITS'] = size_frame['MCCsize'].divide(size_frame['ITSsize'], fill_value=0)
+        """
         #NaN/Number = 0 --> 1000
         size_df[size_df == 0] = 1000
         #Number/NaN = inf --> 0
+        size_df.fillna(1,inplace=True)
         size_df.replace([np.inf, -np.inf], 0, inplace=True)
+        size_df = size_df[(size_df.T != 1).any()]
+        #size_df = size_df[(size_df['A+B/ITS'] <= 0.7) & (size_df['A+B/MCC'] <= 0.7) & (size_df['A+B/Spike'] <= 0.7)]
+        #size_df.to_csv('test.csv')
 
-        make_plot(size_df, options.numCases, options.worstCases, size_df.columns, 'Size ratio', options.sizeOut,options.show)
+        make_plot(size_df, options.numCases, options.worstCases, size_df.columns, 'Size ratio', options.sizeOut,options.show, ['b', 'r', 'k', 'g','m', 'lime'],['--', ':', '-.', '-', ':', '--', '-.'], ymax=10)
     
     
 
